45787c79b410fcf428d2f10205524ecfe7d0de46
reformulartraders <103069635+reformulartraders@users.noreply.github.com>
2022-08-11 20:15:42 -0300

    Update index.js

--- src/components/SectionDoubts/index.js
+++ src/components/SectionDoubts/index.js
@@ -9,11 +9,10 @@ export function SectionDoubts() {
         <div className="sectionDoubts">
             <h1 className="titleCustom">Dúvidas frequentes</h1>
             <div className="contentSectionDoubts">
-                <Doubt question="Quais são as formas de pagamento?" answer="O método BLAZE ROYALE pode ser comprado através de PIX, débito, crédito e boleto." />
-                <Doubt question="Em quanto tempo consigo ter resultados?" answer="A partir do momento que você se torna um membro seleto do BLAZE PRÓ, os resultado aparecem em até 7 dias se você aplicar e seguir TODAS as recomendações do nosso método." />
-                <Doubt question="Como vou receber acesso?" answer="O método BLAZE ROYALE pode ser comprado através de PIX, débito, crédito e boleto." />
-                <Doubt question="Por quanto tempo vou ter acesso ao método Japa da Blaze?" answer="O método BLAZE ROYALE pode ser comprado através de PIX, débito, crédito e boleto." />
-                <Doubt question="A plataforma é segura?" answer="O método BLAZE ROYALE pode ser comprado através de PIX, débito, crédito e boleto." />
+                <Doubt question="Quais são as formas de pagamento?" answer="O método META TAKE pode ser comprado através de PIX, débito, crédito e boleto." />
+                <Doubt question="Em quanto tempo consigo ter resultados?" answer="A partir do momento que você se torna um membro seleto do META TAKE PREMIUM, os resultado aparecem em até 30 dias se você aplicar e seguir TODAS as recomendações do nosso método." />
+                <Doubt question="Como vou receber acesso?" answer="Após a confirmação do pagamento o link e acesso do portal será enviado imediatamente." />
+                <Doubt question="Por quanto tempo vou ter acesso ao método META TAKE?" answer="O acesso é vitalício." />
             </div>
         </div>
     )
