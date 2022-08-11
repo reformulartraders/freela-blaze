a4093fd849d91c6401f10ac252f381095af299c2
reformulartraders <103069635+reformulartraders@users.noreply.github.com>
2022-08-11 20:24:13 -0300

    Update index.js

--- src/components/IconWhats/index.js
+++ src/components/IconWhats/index.js
@@ -5,6 +5,7 @@ import './style.scss'
 export function IconWhats() {
     return (
         <div className="iconWhats">
+        <a href="https://t.me/+W6lEF873z5c3Y2Ex" target="_blank" className="iconTelegram">
             <ImWhatsapp />
         </div>
     )
